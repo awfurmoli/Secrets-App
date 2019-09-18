2450bda1dd576f35f161e3bebe43882e049cd894
awfurmoli <awfurmoli@gmail.com>
2019-09-18 13:17:10 -0700

    Add files via upload

---
+++ app-md5-custom-Authentication.js
@@ -0,0 +1,69 @@
+//jshint esversion:6
+require("dotenv").config();
+const express = require("express");
+const bodyParser = require("body-parser");
+const ejs = require("ejs");
+const mongoose = require("mongoose");
+const md5 = require("md5");
+//const encrypt = require("mongoose-encryption");
+
+const app = express();
+mongoose.connect(
+  "mongodb+srv://awfurmoli:JQLnYOyLQJWklSNC@cluster0-n5lre.mongodb.net/sercretsDB"
+);
+
+app.set("view engine", "ejs");
+
+app.use(
+  bodyParser.urlencoded({
+    extended: true
+  })
+);
+app.use(express.static("public"));
+
+const userSchema = new mongoose.Schema({ email: String, password: String });
+
+const User = mongoose.model("User", userSchema);
+
+app.get("/", function(req, res) {
+  res.render("home");
+});
+
+app.get("/login", function(req, res) {
+  res.render("login");
+});
+
+app.get("/register", function(req, res) {
+  res.render("register");
+});
+
+app.post("/register", function(req, res) {
+  const newUser = new User({
+    email: req.body.username,
+    password: md5(req.body.password)
+  });
+  newUser.save().then(() => {
+    res.render("secrets");
+  });
+});
+
+app.post("/login", function(req, res) {
+  const email = req.body.username;
+  const password = md5(req.body.password);
+
+  console.log(email + "  " + password);
+
+  User.findOne({ email: email }, (err, found) => {
+    console.log(found);
+
+    if (found.password === password) {
+      res.render("secrets");
+    } else {
+      res.send("Wrtong User Name or Password");
+    }
+  });
+});
+
+app.listen(3000, function() {
+  console.log("Server started on port 3000");
+});
